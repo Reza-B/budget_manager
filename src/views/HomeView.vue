6bb79ad9edf4f807c1a71e57ad35658bead3af48
Mors <seyedsahel1383@gmail.com>
2024-08-31 18:54:57 +0330

    create balance componnent

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,14 @@
 import HomeCard from "@/components/HomeCard.vue";
 import HomeAdd from "@/components/HomeAdd.vue";
 import bankcardicon from "@/assets/icons/bankcardicon.svg" ;
+import HomeBalance from "@/components/HomeBalance.vue";
 
 </script>
 
 <template>
+    <HomeBalance />
     <div class="flex flex-wrap justify-center ">
+    
   <HomeCard :icon = "bankcardicon" title = "cart" value = "25000$"/>
   <HomeCard :icon = "bankcardicon" title = "cash" value = "25000$"/>
   <HomeCard :icon = "bankcardicon" title = "saving" value = "25000$"/>
