e6d9ea6d7a806ba8f22a1b3eec936e5c768cc475
Mors <seyedsahel1383@gmail.com>
2024-09-01 15:52:55 +0330

    craete Analysis Categories

--- src/views/AnalysisView.vue
+++ src/views/AnalysisView.vue
@@ -1,5 +1,8 @@
 <script setup>
 import AnalysisTimes from "@/components/AnalysisTimes.vue";
+import AnalysisCategories from "@/components/AnalysisCategories.vue";
+import bankcardicon from "@/assets/icons/bankcardicon.svg"
+const paymentMetod = ["cash","cart"]
 </script>
 
 <template>
@@ -7,5 +10,12 @@ import AnalysisTimes from "@/components/AnalysisTimes.vue";
     <AnalysisTimes title = "Day" value = "40"/>
     <AnalysisTimes title = "Week" value = "500"/>
     <AnalysisTimes title = "Month" value = "1,612"/>
-    </div>
+</div>
+<div class="flex-col">
+<AnalysisCategories title="Shoping" :icon="bankcardicon" :paymentMetod="paymentMetod" amount="100" percentage="20"/> 
+<AnalysisCategories title="Gifts" :icon="bankcardicon" :paymentMetod="paymentMetod" amount="100" percentage="20"/> 
+<AnalysisCategories title="Food" :icon="bankcardicon" :paymentMetod="paymentMetod" amount="100" percentage="20"/> 
+<AnalysisCategories></AnalysisCategories>
+</div>
 </template>
+
