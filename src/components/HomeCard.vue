a280734dbc38411f77062e5106544656cc26cde6
reza buzarjemehri <dev.reza21@gmail.com>
2024-09-02 18:27:03 +0330

    fix spaces

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -1,50 +1,57 @@
-
 <script setup>
-
-
-import { defineProps ,computed  } from "vue";
-
-
+import { defineProps, computed } from "vue";
 
 const props = defineProps({
-     icon :{
-        default : '@/assets/icons/bankcardicon.svg'
-     },
-     title :{
-        type : String,
-        default : "title"
-     },
-     value :{
-        type : String,
-        default : "1000$"
-     }
-})
-
-const conditionalClass = computed(() => {
-  switch (props.title) {
-    case 'cash':
-      return 'bg-[#E1ECFE]';
-    case 'cart':
-      return "bg-[#FEE0D6]";
-    case 'saving':
-      return "bg-[#fff2bd]";
-    default:
-      return "bg-[#bdffc8]";
-  }
+	icon: {
+		default: "@/assets/icons/bankcardicon.svg",
+	},
+	title: {
+		type: String,
+		default: "title",
+	},
+	value: {
+		type: String,
+		default: "1000$",
+	},
 });
 
-
+const conditionalClass = computed(() => {
+	switch (props.title) {
+		case "cash":
+			return "bg-[#E1ECFE]";
+		case "cart":
+			return "bg-[#FEE0D6]";
+		case "saving":
+			return "bg-[#fff2bd]";
+		default:
+			return "bg-[#bdffc8]";
+	}
+});
 </script>
 
 <template>
-    <div :class="['w-[45%]','m-2', 'flex-col', 'rounded-[25%]', 'grid', 'grid-rows-2', 'grid-flow-col', conditionalClass]">
-        <div class="justify-between grid grid-cols-2 ">
-            <span class="w-[70px] h-[70px] m-3 bg-white rounded-full flex items-center justify-center"> <img :src="icon"></span>
-
-            <p class=" flex items-center justify-center text-2xl">{{ title }}</p>
-        </div>
-        <div class="flex items-center justify-center ">
-        <p class="text-3xl"><span class="text-xl font-bold text-slate-600">$</span>{{ value }}</p></div>
-    </div>
-  </template>
-  
+	<div
+		:class="[
+			'w-[45%]',
+			'm-2',
+			'flex-col',
+			'rounded-[25%]',
+			'grid',
+			'grid-rows-2',
+			'grid-flow-col',
+			conditionalClass,
+		]">
+		<div class="flex">
+			<span
+				class="w-[70px] h-[70px] m-3 bg-white rounded-full flex items-center justify-center">
+				<img :src="icon"
+			/></span>
+			<p class="flex items-center justify-center text-2xl">{{ title }}</p>
+		</div>
+		<div class="flex items-center justify-center">
+			<p class="text-3xl">
+				<span class="text-xl font-bold text-slate-600">$</span>{{ value }}
+			</p>
+		</div>
+	</div>
+</template>
--- src/views/AnalysisView.vue
+++ src/views/AnalysisView.vue
@@ -1,21 +1,42 @@
 <script setup>
 import AnalysisTimes from "@/components/AnalysisTimes.vue";
 import AnalysisCategories from "@/components/AnalysisCategories.vue";
-import bankcardicon from "@/assets/icons/bankcardicon.svg"
-const paymentMetod = ["cash","cart"]
+import shopicon from "@/assets/icons/shopicon.svg";
+import gifticon from "@/assets/icons/gifticon.svg";
+import foodicon from "@/assets/icons/foodicon.svg";
+const paymentMetod = ["cash", "cart"];
 </script>
 
 <template>
-    <div class="flex flex-wrap justify-center ">
-    <AnalysisTimes title = "Day" value = "40"/>
-    <AnalysisTimes title = "Week" value = "500"/>
-    <AnalysisTimes title = "Month" value = "1,612"/>
-</div>
-<div class="flex-col">
-<AnalysisCategories title="Shoping" :icon="bankcardicon" :paymentMetod="paymentMetod" amount="100" percentage="20"/> 
-<AnalysisCategories title="Gifts" :icon="bankcardicon" :paymentMetod="paymentMetod" amount="100" percentage="20"/> 
-<AnalysisCategories title="Food" :icon="bankcardicon" :paymentMetod="paymentMetod" amount="100" percentage="20"/> 
-<AnalysisCategories></AnalysisCategories>
-</div>
+	<div class="flex flex-wrap justify-center">
+		<AnalysisTimes
+			title="Day"
+			value="40" />
+		<AnalysisTimes
+			title="Week"
+			value="500" />
+		<AnalysisTimes
+			title="Month"
+			value="1,612" />
+	</div>
+	<div class="flex-col">
+		<AnalysisCategories
+			title="Shoping"
+			:icon="shopicon"
+			:paymentMetod="paymentMetod"
+			amount="100"
+			percentage="20" />
+		<AnalysisCategories
+			title="Gifts"
+			:icon="gifticon"
+			:paymentMetod="paymentMetod"
+			amount="100"
+			percentage="20" />
+		<AnalysisCategories
+			title="Food"
+			:icon="foodicon"
+			:paymentMetod="paymentMetod"
+			amount="100"
+			percentage="20" />
+	</div>
 </template>
-
